e51670c183e803cd6e0f22e94e4c6b39bd80e986
kamrul1275 <kamrul1275.info@gmail.com>
2024-02-04 14:38:40 +0600

    crud done

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <nuxt-link to="/" class="nav-link active" aria-current="page">Home</nuxt-link>
+        </li>
+        <li class="nav-item">
+            <nuxt-link to="/product" class="nav-link active" aria-current="page">Product</nuxt-link>
+        </li>
+        <li class="nav-item">
+            <nuxt-link to="/about" class="nav-link active" aria-current="page">About</nuxt-link>
+        </li>
+       
+       
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+
+
+
+
+
+
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
+          
+        }
+      ],
+
+
+      script:[
+
+        {
+        src: "https://code.jquery.com/jquery-3.7.1.min.js",
+        type:'text/javascript'
+
+        },
+
+          {
+            src:'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+            type:'text/javascript'
+          }
+
+      ]
+
+
+
+    }
+  }
+})
